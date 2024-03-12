109c4005d1e064859d66fbfbc078ed186976ff02
Kareem Hatem <124918491+KareemHatem013@users.noreply.github.com>
2024-03-12 23:58:34 +0200

    Articulation_Points.cpp

---
+++ Articulation_Points.cpp
@@ -0,0 +1,29 @@
+const int N = 2e5+5;
+vector<int>adj[N];
+int dfn[N],low[N];
+set<int>points;
+int cnt;
+bool root;
+//undirected
+//low[i] = dfn[i] = -1
+//root = false
+//Articulation Points
+void dfs(int node, int parent){
+    dfn[node] = low[node] = ++cnt;
+    for(auto&ch : adj[node]){
+        if(ch == parent)continue;
+        if(dfn[ch] == -1){
+            dfs(ch, node);
+            low[node] = min(low[node],low[ch]);
+            if(~parent){
+                if(dfn[node] <= low[ch])
+                    points.insert(node);
+            }else {
+                if(!root)root = true;
+                else points.insert(node);
+            }
+        }else{
+            low[node] = min(low[node],dfn[ch]);
+        }
+    }
+}
