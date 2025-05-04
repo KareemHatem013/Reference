1dec017958e1fc521863461e21ffab1fe2c50947
Kareem Hatem <124918491+KareemHatem013@users.noreply.github.com>
2025-05-04 10:48:37 +0300

    Create KMP.cpp

---
+++ KMP.cpp
@@ -0,0 +1,13 @@
+vector<int>KMP(const string&s){
+    int n = sz(s);
+    vector<int>fail(n);
+    for(int i = 1;i<n;i++){
+        int j = fail[i-1];
+        while(j && s[i]!=s[j]){
+            j = fail[j-1];
+        }
+        if(s[i] == s[j])j++;
+        fail[i] = j;
+    }
+    return fail;
+}
