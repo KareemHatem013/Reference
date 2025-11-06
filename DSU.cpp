fdb922f83718decc8884d2f635b64157e6607973
Kareem Hatem <124918491+KareemHatem013@users.noreply.github.com>
2025-11-06 14:32:24 +0200

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,30 +1,29 @@
 struct DSU {
     vector<int> parent, size;
+    int comp;
 
     DSU(int n) {
         parent = size = vector<int>(n + 1);
+        comp = n;
         for (int i = 0; i <= n; i++) {
             parent[i] = i;
             size[i] = 1;
         }
     }
 
-    int FindLeader(int i) {
+    int Leader(int i) {
         if (parent[i] == i) return i;
-        return parent[i] = FindLeader(parent[i]);
+        return parent[i] = Leader(parent[i]);
     }
 
-    bool MergeGroups(int x, int y) {
-        int leader1 = FindLeader(x);
-        int leader2 = FindLeader(y);
-        if (leader1 == leader2) return false;
-        if (size[leader1] > size[leader2]) {
-            parent[leader2] = leader1;
-            size[leader1] += size[leader2];
-        } else {
-            parent[leader1] = leader2;
-            size[leader2] += size[leader1];
-        }
+    bool Merge(int x, int y) {
+        int l1 = Leader(x);
+        int l2 = Leader(y);
+        if (l1 == l2) return false;
+        comp--;
+        if (size[l1] < size[l2])swap(l1, l2), swap(x, y);
+        parent[l2] = l1;
+        size[l1] += size[l2];
         return true;
     }
 };
